00ef51cbdfb39d095eb1bbc7159cb89fae43122f
Melanie Brown <brown.m@pm.me>
2024-02-11 15:49:57 -0500

    unused function

--- lib/Language/Hott/Monad.hs
+++ lib/Language/Hott/Monad.hs
@@ -6,7 +6,6 @@ module Language.Hott.Monad
   , module Control.Monad.Except
   , module Control.Monad.State
   , module Data.Functor
-  , guardError
   )
 where
 
@@ -14,7 +13,6 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.Except
 import Control.Monad.State
-import Data.Bool (bool)
 import Data.Functor
 import Data.Kind (Constraint, Type)
 
@@ -22,9 +20,6 @@ type MonadFresh :: (Type -> Type) -> Constraint
 class (Monad m) => MonadFresh m where
   type Fresh m :: Type
   fresh :: m (Fresh m)
-
-guardError :: (MonadError e m) => e -> Bool -> m ()
-guardError e = bool (throwError e) (pure ())
 
 type MonadContext :: (Type -> Type) -> Constraint
 class MonadContext m where
